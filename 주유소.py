e682aca82ae1937c81e76bb413cff76423949589
Factories-git <official.factories@gmail.com>
2025-01-05 20:23:28 +0900

    Changes

---
+++ 주유소.py
@@ -0,0 +1,35 @@
+import heapq
+
+
+def dijkstra(g, start):
+    re = [0] * n
+    distances = [float('inf')] * len(g)
+    distances[start] = 0
+    pq = [(0, start)]
+    while pq:
+        curr_dist, curr_vert = heapq.heappop(pq)
+        if curr_vert == target:
+            return [curr_dist , re]
+        if curr_dist > distances[curr_vert]:
+            continue
+        for vertex, weight in g[curr_vert]:
+            new_dist = curr_dist + weight
+            if new_dist < distances[vertex]:
+                distances[vertex] = new_dist
+                heapq.heappush(pq, (new_dist, vertex))
+                re[vertex] = vertex
+        #바로 위는 디버깅용, 바로 아래는 정답용
+
+
+n, m = map(int, input().split())
+g = [[] * n for _ in range(m)]
+oils = []
+money = list(map(int, input().split()))
+for i in range(m):
+    f, s, l = map(int, input().split())
+    g[f - 1].append((s - 1, l))
+    g[s - 1].append((f - 1, l))
+start = 0
+target = n - 1
+distances = dijkstra(g, start)
+print(distances)
